2abe29f998f4d132680768169608a24904f75262
Angel-dash <njlghmr@gmail.com>
2023-08-11 20:46:31 +0545

    adding validation

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -26,7 +26,13 @@ class _NewItemState extends State<NewItem> {
                   label: Text("Name"),
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 charecters';
+                  }
+                  return null;
                 },
               ),
               Row(
@@ -34,10 +40,20 @@ class _NewItemState extends State<NewItem> {
                 children: [
                   Expanded(
                     child: TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text("Quantity"),
-                        ),
-                        initialValue: '1'),
+                      decoration: const InputDecoration(
+                        label: Text("Quantity"),
+                      ),
+                      initialValue: '1',
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid postive number';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
                   const SizedBox(
                     width: 8,
